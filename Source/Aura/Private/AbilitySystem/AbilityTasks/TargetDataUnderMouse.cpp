5f802d93a194f9de66859244464b99fbeec34857
laddieyao <15869790915@163.com>
2024-10-13 20:33:22 +0800

    118.Receiving Target Dat

--- Source/Aura/Private/AbilitySystem/AbilityTasks/TargetDataUnderMouse.cpp
+++ Source/Aura/Private/AbilitySystem/AbilityTasks/TargetDataUnderMouse.cpp
@@ -17,11 +17,29 @@ void UTargetDataUnderMouse::Activate()
 {
 	if (const bool bIsLocallyControlled = IsLocallyControlled())
 	{
+		// SendTargetDataToServer
 		SendMouseCursorData();
 	}
+	// 服务端
 	else
 	{
+		// ReceiveTargetDataFromClient
+		/*
+		 *	此时会有俩种情况
+		 *	1. RPC传送到服务端的事件 “早于” 数据传送到服务端的时间
+		 *	2. RPC传送到服务端的事件 “晚于” 数据传送到服务端的时间
+		 */
 		//TODO: We are on the server, so listen for target data.
+		const FGameplayAbilitySpecHandle SpecHandle = GetAbilitySpecHandle();
+		const FPredictionKey ActivationPredictionKey = GetActivationPredictionKey();
+		AbilitySystemComponent.Get()->AbilityTargetDataSetDelegate(SpecHandle, ActivationPredictionKey).AddUObject(this, &UTargetDataUnderMouse::OnTargetDataReplicatedCallback);
+		// 检查数据是否收到，如果已经收到数据，广播委托上面的委托，调用 OnTargetDataReplicatedCallback 回调函数
+		const bool bCalledDelegate = AbilitySystemComponent.Get()->CallReplicatedTargetDataDelegatesIfSet(SpecHandle, ActivationPredictionKey);
+		// 如果数据还未收到，在适当的时机终止
+		if (!bCalledDelegate)
+		{
+			SetWaitingOnRemotePlayerData();
+		}
 	}
 	
 }
@@ -48,6 +66,24 @@ void UTargetDataUnderMouse::SendMouseCursorData()
 		AbilitySystemComponent->ScopedPredictionKey);
 
 	// 确保相关的Ability在活动状态时才进行广播
+	if (ShouldBroadcastAbilityTaskDelegates())
+	{
+		ValidData.Broadcast(DataHandle);
+	}
+}
+
+
+/**
+ * 该回调函数绑定到
+ * [ AbilitySystemComponent.Get()->AbilityTargetDataSetDelegate(SpecHandle, ActivationPredictionKey) ]
+ * 返回的委托，这个委托是给定 Ability/PredictionKey 对的 TargetDataSet 委托
+ * @param DataHandle 
+ * @param ActivationTag 
+ */
+void UTargetDataUnderMouse::OnTargetDataReplicatedCallback(const FGameplayAbilityTargetDataHandle& DataHandle,
+	FGameplayTag ActivationTag)
+{
+	AbilitySystemComponent->ConsumeClientReplicatedTargetData(GetAbilitySpecHandle(), GetActivationPredictionKey());
 	if (ShouldBroadcastAbilityTaskDelegates())
 	{
 		ValidData.Broadcast(DataHandle);
